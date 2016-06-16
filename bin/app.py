e5a86e08bb572cd6c6c1a0b3fb09e06c36c78c9e
huckleberrych <sinai462003@yahoo.com>
2016-06-16 16:17:47 -0400

    test

--- bin/app.py
+++ bin/app.py
@@ -56,6 +56,6 @@ class Index(object):
                                 diff['Solomon'])
                                 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
-    #app.run()
+    #port = int(os.environ.get('PORT', 8080))
+    #app.run(host='0.0.0.0', port=port)
+    app.run()
